8b0ee44c018637b2e849dafb45e813e33271db77
gregorioazevedo <gregorioazevedo.dev@gmail.com>
2023-05-30 00:05:08 -0300

    Created the clock features
    Now it shows hh:mm:ss in real time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import concatZero from './concatZero';
+
 const App = function () {
-  return <h1>Hello World</h1>;
+  let time;
+  time = new Date();
+
+  const [hours, setHours] = useState(time.getHours());
+  const [minutes, setMinutes] = useState(time.getMinutes());
+  const [seconds, setSeconds] = useState(time.getSeconds());
+
+  setInterval(() => {
+    const updateTime = new Date();
+
+    setHours(concatZero(updateTime.getHours()));
+    setMinutes(concatZero(updateTime.getMinutes()));
+    setSeconds(concatZero(updateTime.getSeconds()));
+  }, 1000);
+
+  return (
+    <section id="clock">
+      <p>
+        {hours}:{minutes}:{seconds}
+      </p>
+
+      <article className="hours">
+        <div id="tenHours" className="clockBar reduced"></div>
+        <div id="unitHour" className="clockBar"></div>
+      </article>
+      <article className="minutes">
+        <div id="tenMinutes" className="clockBar reduced"></div>
+        <div id="unitMinutes" className="clockBar"></div>
+      </article>
+      <article className="seconds">
+        <div id="tenSeconds" className="clockBar reduced"></div>
+        <div id="unitSeconds" className="clockBar"></div>
+      </article>
+    </section>
+  );
 };
 
 export default App;
